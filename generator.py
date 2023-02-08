b226a4333a68edde760bdaa592711a129ddf93b6
Devli-T <67900268+Devli-T@users.noreply.github.com>
2023-02-08 16:29:06 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -38,15 +38,28 @@ class Enemy(pygame.sprite.Sprite):
         self.health = 1
 
     # Method to move the enemy towards the player
-    def move_towards_player(self, Player):
+    def move_towards_player(self, Player, death):
+        a = Player.rect.x + 20
+        b = Player.rect.y + 40
         # Calculate the distance between the enemy and the player
-        distance = ((self.rect.x - Player.rect.x) ** 2 + (self.rect.y - Player.rect.y) ** 2) ** 0.5
+        dx = self.rect.x - a
+        dy = self.rect.y - b
+        distance = ((dx ** 2) + (dy ** 2)) ** 0.5
 
-        # If the distance is less than 20 pixels, move the enemy towards the player
+        # If the distance is less than 900 pixels, move the enemy towards the player
         if distance < 900:
-            angle = math.atan2(Player.rect.y - self.rect.y, Player.rect.x - self.rect.x)
-            self.rect.x += math.cos(angle)
-            self.rect.y += math.sin(angle)
+            dx = min(max(dx, -1), 1)
+            dy = min(max(dy, -1), 1)
+
+            self.rect.x -= dx
+            self.rect.y -= dy
+        if self.rect.x == a and self.rect.y == b:
+            self.health -= 1
+            Player.health -= 1
+            Player.rect.x += 5
+            Player.rect.y += 5
+            death += 1
+        return death
 
     def update(self, Player):
         if abs(self.x - Player.rect.x) <= 2 and abs(self.y - Player.rect.y) <= 2:
@@ -74,20 +87,19 @@ class Fireball:
         self.direction = 1
         self.image = pygame.transform.scale(pygame.image.load("fireball.png"), (20, 20))
 
-def create_enemies(World_Map, enemy_image, time):
+def create_enemies(World_Map, enemy_image):
     enemies = pygame.sprite.Group()
     ran = [True, False]
     for y, row in enumerate(World_Map):
         for x, char in enumerate(row):
             # Position calculation based on the dimensions of the enemy sprite
             # and the size of the cells in the World_Map
-            if char == "v" and np.random.choice(ran, p=[0.1, 0.9]):
+            if char == "v" and np.random.choice(ran, p=[3/46, 43/46]):
                 enemies.add(Enemy(x*40 + 20, y*40 + 20, enemy_image))
+    for enemy in enemies:
+        enemy.health = 1
     return enemies
 
 def draw_hearts(screen, player_health, heart_image):
     for i in range(player_health):
         screen.blit(heart_image, (10 + i * 30, 10))
-
-
-
